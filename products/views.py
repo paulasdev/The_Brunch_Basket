656f742bd77fb471b209e0ef76bb21624b8771db
Paula da Silva <carolpaulag@gmail.com>
2023-04-11 14:37:51 +0000

    Added sorting and product counts to product page

--- products/views.py
+++ products/views.py
@@ -22,7 +22,8 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
                 if direction == 'desc':
@@ -65,4 +66,3 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
-
