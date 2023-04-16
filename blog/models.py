938efcf729e202128c3c4e3986c571de6de658be
Paula da Silva <carolpaulag@gmail.com>
2023-04-16 21:05:03 +0000

    create blog app and models

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,41 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from django.urls import reverse
+
+
+STATUS_CHOICES = (
+    (0, "Draft"),
+    (1, "Publish")
+)
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
 
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
-
-    def get_absolute_url(self):
-        return reverse('post_detail', args=[str(self.id)])
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.title
 
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
     def __str__(self):
-        return self.title
+        return f"Comment {self.body} by {self.name}"
