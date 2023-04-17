b0cebda87a328562e53b78d8e76e6cc0c290c39e
Paula da Silva <carolpaulag@gmail.com>
2023-04-17 16:48:38 +0000

    delete the app

--- newsletter/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Newsletter(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
