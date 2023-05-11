11c159751832a3069500fb68c14f7076360dafad
Paula da Silva <carolpaulag@gmail.com>
2023-05-11 12:27:14 +0000

    Fixed the function add_to_bag

--- bag/views.py
+++ bag/views.py
@@ -15,30 +15,18 @@ def view_bag(request):
 
 
 def add_to_bag(request, item_id):
-    """ Add a quantity of the specified product to the shopping bag """
-
+    """Add a quantity of the specified product to the shopping bag."""
+    bag = request.session.get('bag', {})
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
-    if size:
-        if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_size'].keys():
-                bag[item_id]['items_by_size'][size] += quantity
-                messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
-            else:
-                bag[item_id]['items_by_size'][size] = quantity
-                messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
-        else:
-            bag[item_id] = {'items_by_size': {size: quantity}}
-            messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
+    if item_id in bag:
+        bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}.')
     else:
-        if item_id in list(bag.keys()):
-            bag[item_id] += quantity
-            messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
-        else:
-            bag[item_id] = quantity
-            messages.success(request, f'Added {product.name} to your bag')
+        bag[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your bag.')
 
     request.session['bag'] = bag
     return redirect(redirect_url)
