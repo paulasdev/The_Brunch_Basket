89a873c1ee82b8b9ec370c12147715a336327966
Paula da Silva <carolpaulag@gmail.com>
2023-05-21 12:46:26 +0000

    sending real emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -135,7 +135,6 @@ class StripeWH_Handler:
                                 order=order,
                                 product=product,
                                 quantity=quantity,
-                                product_size=size,
                             )
                             order_line_item.save()
             except Exception as e:
--- the_brunch_basket/settings.py
+++ the_brunch_basket/settings.py
@@ -30,7 +30,7 @@ DEBUG = 'DEVELOPMENT' in os.environ
 # DEBUG = False
 
 
-ALLOWED_HOSTS = ['the-brunch-basket.herokuapp.com', '8000-paulasdev-thebrunchbask-biaqes2hpeo.ws-eu97.gitpod.io','localhost']
+ALLOWED_HOSTS = ['the-brunch-basket.herokuapp.com', 'localhost']
 
 
 
@@ -47,7 +47,6 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'django_summernote',
     'home',
     'products',
     'bag',
@@ -221,6 +220,16 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'thebrunchbasket@example.com'
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'DEVELOPMENT' in os.environ: 
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'thebrunchbasket@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
