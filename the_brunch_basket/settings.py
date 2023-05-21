5c689e1722c08ae930e9ddb5888eaf3537102103
Paula da Silva <carolpaulag@gmail.com>
2023-05-21 15:04:04 +0000

    fixed error on settings and blog model

--- the_brunch_basket/settings.py
+++ the_brunch_basket/settings.py
@@ -220,6 +220,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 if 'DEVELOPMENT' in os.environ: 
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
