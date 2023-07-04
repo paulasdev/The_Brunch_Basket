ccffc0b2fbd8471ce72f97e415b4aab2e4bbc85d
Paula da Silva <carolpaulag@gmail.com>
2023-07-04 06:24:29 +0000

    Customizing error views - fix deployment error3

--- the_brunch_basket/views.py
+++ the_brunch_basket/views.py
@@ -8,7 +8,7 @@ def handler404(request, exception):
     return render(request, "errors/404.html", status=404)
 
 
-def handler500(request, exception):
+def handler500(request):
     """
     Custom error 500 - Server Error
     """
