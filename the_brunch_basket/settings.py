76f9e3be65b9e077ebdb3b842d595b6d193ae9b6
Paula da Silva <carolpaulag@gmail.com>
2023-07-16 01:21:49 +0000

    completed confirmation email

--- the_brunch_basket/settings.py
+++ the_brunch_basket/settings.py
@@ -223,6 +223,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'thebrunchbasket@example.com'
